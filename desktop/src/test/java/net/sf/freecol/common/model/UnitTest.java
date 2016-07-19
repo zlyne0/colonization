f7d5526175a3870840be9c292f03ffcb17906c72
Space Invader <ne0@poczta.onet.pl>
2016-07-19 12:46:38 +0200

    navy unit initial moves

--- desktop/src/test/java/net/sf/freecol/common/model/UnitTest.java
+++ desktop/src/test/java/net/sf/freecol/common/model/UnitTest.java
@@ -1,68 +1,62 @@
-package net.sf.freecol.common.model.player;
+package net.sf.freecol.common.model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglFiles;
 
-import net.sf.freecol.common.model.Game;
-import net.sf.freecol.common.model.Specification;
-import net.sf.freecol.common.model.Unit;
+import net.sf.freecol.common.model.player.Player;
 import net.sf.freecol.common.model.specification.FoundingFather;
 import promitech.colonization.savegame.SaveGameParser;
 
-public class HighSeasTest {
+public class UnitTest {
 
+    Game game; 
+    
     @BeforeClass
     public static void beforeClass() {
         Gdx.files = new LwjglFiles();
     }
-    
-    @Test
-    public void canCalculateSailTurnsWithoutFerdinandMagellan() throws Exception {
-        // given
-        SaveGameParser saveGameParser = new SaveGameParser("maps/savegame_1600_for_jtests.xml");
-        Game game = saveGameParser.parse();
-        
-        Player player = game.players.getById("player:1");
-        assertNull("should not have father", player.foundingFathers.getByIdOrNull(FoundingFather.FERDINAND_MAGELLAN));
-        
-        Unit merchantman = player.units.getById("unit:6437");
-        
-        
-        HighSeas highSeas = player.getHighSeas();
-        
-        // when
-        int sailTurns = highSeas.getSailTurns(merchantman);
 
-        // then
-        assertEquals(3, sailTurns);
+    @Before
+    public void before() throws Exception {
+        SaveGameParser saveGameParser = new SaveGameParser("maps/savegame_1600_for_jtests.xml");
+        game = saveGameParser.parse();
     }
 
     @Test
-    public void canCalculateSailTurnsWithFerdinandMagellan() throws Exception {
+    public void canCalculateInitialMovesForNavyWithoutFerdinandMagellan() {
         // given
-        SaveGameParser saveGameParser = new SaveGameParser("maps/savegame_1600_for_jtests.xml");
-        Game game = saveGameParser.parse();
-
+        Player player = game.players.getById("player:1");
+        assertNull("should not have father", player.foundingFathers.getByIdOrNull(FoundingFather.FERDINAND_MAGELLAN));
+        Unit merchantman = player.units.getById("unit:6437");
+        
+        // when
+        int initialMovesLeft = merchantman.getInitialMovesLeft();
+        
+        // then
+        assertEquals(15, initialMovesLeft);
+    }
+    
+    @Test
+    public void canCalculateInitialMovesForNavyWithFerdinandMagellan() {
+        // given
         Player player = game.players.getById("player:1");
         if (player.foundingFathers.getByIdOrNull(FoundingFather.FERDINAND_MAGELLAN) == null) {
             FoundingFather foundingFather = Specification.instance.foundingFathers.getById(FoundingFather.FERDINAND_MAGELLAN);
             player.addFoundingFathers(foundingFather);
         }
-        
         Unit merchantman = player.units.getById("unit:6437");
         
-        HighSeas highSeas = player.getHighSeas();
-        
         // when
-        int sailTurns = highSeas.getSailTurns(merchantman);
-
+        int initialMovesLeft = merchantman.getInitialMovesLeft();
+        
         // then
-        assertEquals(2, sailTurns);
+        assertEquals(18, initialMovesLeft);
     }
     
 }
