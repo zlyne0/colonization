6c5aa72c4f296b63aab3d0a40c40c41b1aee0f36
Space Invader <ne0@poczta.onet.pl>
2017-01-14 23:23:38 +0100

    refactoring ai exploration

--- core/src/promitech/colonization/ai/AILogic.java
+++ core/src/promitech/colonization/ai/AILogic.java
@@ -1,25 +1,25 @@
 package promitech.colonization.ai;
 
 import net.sf.freecol.common.model.Game;
-import net.sf.freecol.common.model.Tile;
+import net.sf.freecol.common.model.MapIdEntities;
 import net.sf.freecol.common.model.Unit;
 import net.sf.freecol.common.model.map.PathFinder;
 import net.sf.freecol.common.model.player.Player;
-import promitech.colonization.Direction;
 import promitech.colonization.GameLogic;
-import promitech.colonization.gamelogic.MoveContext;
 
 public class AILogic {
 
 	private final Game game;
 	private final GameLogic gameLogic;
 	private final PathFinder pathFinder = new PathFinder();
-	private final NavyExplorer navyExplorer;
+	
+	private final ExplorerMissionHandler explorerMissionHandler;
 	
 	public AILogic(Game game, GameLogic gameLogic) {
 		this.game = game;
 		this.gameLogic = gameLogic;
-		navyExplorer = new NavyExplorer(game.map);
+		
+		explorerMissionHandler = new ExplorerMissionHandler(game, pathFinder);
 	}
 	
 	public void aiNewTurn(Player player) {
@@ -29,68 +29,19 @@ public class AILogic {
 			return;
 		}
 		if (player.isLiveEuropeanPlayer()) {
-			exploreSea(player);
-		}
-	}
-
-	private void exploreSea(Player player) {
-		Unit ship = findNavy(player);
-		if (ship == null) {
-			return;
-		}
-		if (!ship.hasMovesPoints()) {
-			return;
-		}
-		pathFinder.generateRangeMap(game.map, ship.getTile(), ship);
-		navyExplorer.generateExploreDestination(pathFinder, ship.getOwner());
-		
-        if (!navyExplorer.isFoundExploreDestination()) {
-        	// maybe is everything explored or blocked in some how
-        	System.out.println("can not find tile to explore");
-        	return;
-        }
-        
-        if (navyExplorer.isExploreDestinationInOneTurn()) {
-        	exploreByOneMove(ship, player);
-        } else {
-        	System.out.println("exploration path " + navyExplorer.getExploreDestinationAsPath());
-        }
-	}
-
-	public void exploreByOneMove(Unit ship, Player player) {
-    	boolean canHandleMove = false;
-        do {
-            Direction direction = navyExplorer.getExploreDestinationAsDirection();
-            System.out.println("exploration destination " + direction);
-            
-            Tile sourceTile = ship.getTile();            
-			Tile destTile = game.map.getTile(sourceTile.x, sourceTile.y, direction);
-			MoveContext moveContext = new MoveContext(sourceTile, destTile, ship, direction);
-           
-			canHandleMove = moveContext.canHandleMove();
-			if (canHandleMove) {
-				moveContext.handleMove();
-				ship.getOwner().revealMapAfterUnitMove(game.map, ship);
-				
-				pathFinder.generateRangeMap(game.map, ship.getTile(), ship);
-				navyExplorer.generateExploreDestination(pathFinder, ship.getOwner());
-				if (navyExplorer.isFoundExploreDestination() && navyExplorer.isExploreDestinationInOneTurn()) {
-					
-				} else {
-					System.out.println("end move points for player " + player);
-					break;
+			
+			// create missions
+			MapIdEntities<ExplorerMission> missions = new MapIdEntities<ExplorerMission>();
+			for (Unit unit : player.units.entities()) {
+				if (unit.isNaval()) {
+					missions.add(new ExplorerMission(unit));
 				}
 			}
-        } while (canHandleMove);
-	}
-	
-	public Unit findNavy(Player player) {
-		for (Unit unit : player.units.entities()) {
-			if (unit.isNaval()) {
-				return unit;
+
+			// execute missions
+			for (ExplorerMission explorerMission : missions.entities()) {
+				explorerMissionHandler.executeMission(explorerMission);
 			}
 		}
-		return null;
 	}
-	
 }
