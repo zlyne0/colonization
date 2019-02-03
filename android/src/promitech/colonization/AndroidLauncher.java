89f42c25435344e8a43d8ce946b16ad741c80c62
Space Invader <ne0@poczta.onet.pl>
2019-02-03 13:33:51 +0100

    android wake lock

--- android/src/promitech/colonization/AndroidLauncher.java
+++ android/src/promitech/colonization/AndroidLauncher.java
@@ -4,12 +4,15 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
 import promitech.colonization.screen.ApplicationScreenManager;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new ApplicationScreenManager(), config);
 	}
